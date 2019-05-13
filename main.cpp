7999b55364984afa0036282b329593581517e685
Alan Wolfe <alan.wolfe@gmail.com>
2019-05-12 21:05:10 -0700

    Starting

---
+++ main.cpp
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <math.h>
+#include <random>
+#include <vector>
+
+class Histogram
+{
+public:
+    Histogram(float min, float max, size_t numBuckets)
+        : m_min(min)
+        , m_max(max)
+        , m_numBuckets(numBuckets)
+        , m_counts(numBuckets, 0)
+    {
+
+    }
+
+    void AddValue(float value)
+    {
+        // TODO: this isn't quite right
+        float percent = (value - m_min) / (m_max - m_min);
+        size_t bucket = size_t(0.5f + percent * float(m_numBuckets));
+        if (bucket >= m_numBuckets - 1)
+            bucket = m_numBuckets - 1;
+        m_counts[bucket]++;
+    }
+
+    float m_min;
+    float m_max;
+    size_t m_numBuckets;
+    std::vector<size_t> m_counts;
+};
+
+template <typename FUNCTION>
+void MetropolisMCMC(const FUNCTION& function, float xmin, float xmax, float xstart, size_t sampleCount)
+{
+    // are we drawing random numbers or getting expected value (integrating?). Maybe both? i dunno...
+
+    Histogram histogram(xmin, xmax, 100);
+
+    FILE* file = nullptr;
+    fopen_s(&file, "out.csv", "w+t");
+
+    std::random_device rd;
+    std::seed_seq fullSeed{ rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd() };
+    std::mt19937 rng(fullSeed);
+
+    std::normal_distribution<float> normalDist(0.0f, 1.0f);
+    std::uniform_real_distribution<float> uniformDist(0.0f, 1.0f);
+
+    float xcurrent = xstart;
+    float ycurrent = function(xstart);
+
+    fprintf(file, "\"%zu\",\"%f\",\"%f\"\n", size_t(0), xcurrent, ycurrent);
+    histogram.AddValue(xcurrent);
+
+    for (size_t sampleIndex = 1; sampleIndex < sampleCount; ++sampleIndex)
+    {
+        float xnext = xstart + normalDist(rng);
+        float ynext = function(xnext);
+
+        // take the new x if y next > ycurrent, or with some probability based on how much worse it is.
+        float A = ynext / ycurrent;
+        if (uniformDist(rng) < A)
+        {
+            xcurrent = xnext;
+            ycurrent = ynext;
+        }
+
+        fprintf(file, "\"%zu\",\"%f\",\"%f\"\n", sampleIndex, xcurrent, ycurrent);
+        histogram.AddValue(xcurrent);
+    }
+
+    // TODO: spit out values into a CSV and do a histogram with it?
+    fclose(file);
+
+    fopen_s(&file, "histogram.csv", "w+t");
+
+    // TODO: put files in an "out" folder
+    for (size_t index = 0; index < histogram.m_numBuckets; ++index)
+    {
+        float percent = float(index) / float(histogram.m_numBuckets - 1);
+        float x = xmin + percent * (xmax - xmin);
+        float y = function(x);
+
+        fprintf(file, "\"%zu\",\"%f\",\"%f\",\"%zu\",\"%f\",\n", index, x, y, histogram.m_counts[index], float(histogram.m_counts[index])/float(sampleCount));
+    }
+
+    fclose(file);
+}
+
+float Sin(float x)
+{
+    if (x < 0.0f || x > 1.0f)
+        return 0.0f;
+    else
+        return sinf(x);
+}
+
+
+int main(int argc, char** argv)
+{
+    MetropolisMCMC(Sin, 0.0f, 1.0f, 0.5f, 100000);
+
+    return 0;
+}
+
+/*
+
+TODO:
+
+* in the histogram, the actual calculated y's sum up to BUCKET_COUNT * 0.45970 (integration of sin(x) from 0 to 1).
+ * the counts sum up to sample count, which can be normalized to a percentage, but if we are looking for a mean, what do we do?
+ * maybe take the mean x and plug it in to get the mean y?
+ ? we could also make our function a PDF by integrating it and dividing by that as a normalization constant. I don't like that though... you have to know too much about the function.
+
+
+* you have to tune how fast you move x around.
+ * could imagine making it smaller over time. simulated annealing style. cooling rate another hyper parameter though.
+ * could do a line search.  Probably should? talk about it in notes on blog i guess.
+
+* show both usage cases?
+ ? you can use this to find the expected value (mean) which lets you integrate
+ ? you can also use this to draw random numbers
+
+? how fast is convergance?
+
+- use this for sampling from a function as if it were a PDF.
+ - maybe a couple 1d examples, and maybe a 2d example?
+ - could use STB to show data.
+
+- this is continuous PDF, but works for discrete PMF too.
+ - i think the main differences are...
+ - 1) when moving to next state, choose randomly from neighbors
+ - 2) Also need a possibility for staying in the same state (else, you can't always possibly be at every state each step). Unsure specific probabilities
+
+ - Metropolis algorithm is simpler due to being able to assume symetry. Metropolis - Hastings allows asymetry. read docs for more info.
+ 
+
+
+ Links:
+ - real good! https://stephens999.github.io/fiveMinuteStats/MH_intro.html
+
+
+Attachments area
+Preview YouTube video Markov Chain Monte Carlo and the Metropolis Alogorithm
+
+https://youtu.be/h1NOS_wxgGg
+https://www.youtube.com/watch?v=3ZmW_7NXVvk
+https://towardsdatascience.com/a-zero-math-introduction-to-markov-chain-monte-carlo-methods-dcba889e0c50
+https://jeremykun.com/2015/04/06/markov-chain-monte-carlo-without-all-the-bullshit/
+
+
+Next: read up on metropolis light transport
+
+ */
