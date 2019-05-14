c04e4906a4d754f8b336f5d84cc69e177a5cb73c
Alan Wolfe <alan.wolfe@gmail.com>
2019-05-13 21:09:55 -0700

    a bit better. I think clamping X ruins it, which is not good.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 #include <math.h>
 #include <random>
 #include <vector>
+#include <array>
 
 inline float Lerp(float a, float b, float t)
 {
     return a * (1.0f - t) + b * t;
 }
 
-inline float Clamp(float v, float min, float max)
+template<typename T>
+inline T Clamp(T v, T min, T max)
 {
     if (v <= min)
         return min;
@@ -32,11 +34,8 @@ public:
 
     void AddValue(float value)
     {
-        // TODO: this isn't quite right
         float percent = (value - m_min) / (m_max - m_min);
-        size_t bucket = size_t(0.5f + percent * float(m_numBuckets));
-        if (bucket >= m_numBuckets - 1)
-            bucket = m_numBuckets - 1;
+        size_t bucket = Clamp<size_t>(size_t(percent * float(m_numBuckets)), 0, m_numBuckets - 1);
         m_counts[bucket]++;
     }
 
@@ -49,13 +48,6 @@ public:
 template <typename FUNCTION>
 void MetropolisMCMC(const FUNCTION& function, float xmin, float xmax, float xstart, float stepSizeSigma, size_t sampleCount)
 {
-    // are we drawing random numbers or getting expected value (integrating?). Maybe both? i dunno...
-
-    Histogram histogram(xmin, xmax, 100);
-
-    FILE* file = nullptr;
-    fopen_s(&file, "out/out.csv", "w+t");
-
     std::random_device rd;
     std::seed_seq fullSeed{ rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd() };
     std::mt19937 rng(fullSeed);
@@ -63,49 +55,89 @@ void MetropolisMCMC(const FUNCTION& function, float xmin, float xmax, float xsta
     std::normal_distribution<float> normalDist(0.0f, stepSizeSigma);
     std::uniform_real_distribution<float> uniformDist(0.0f, 1.0f);
 
-    float xcurrent = xstart;
-    float ycurrent = function(xstart);
-    float expectedValue = ycurrent;
+    FILE* file = nullptr;
 
-    fprintf(file, "\"index\",\"x\",\"y\",\"expected value\"\n");
-    fprintf(file, "\"%zu\",\"%f\",\"%f\",\"%f\"\n", size_t(0), xcurrent, ycurrent, expectedValue);
-    histogram.AddValue(xcurrent);
-
-    for (size_t sampleIndex = 1; sampleIndex < sampleCount; ++sampleIndex)
+    // run our mcmc code, generate and write out the data
+    std::vector<std::array<float, 2>> samples(sampleCount);
+    float ymin = 0.0f;
+    float ymax = 0.0f;
     {
-        float xnext = Clamp(xcurrent + normalDist(rng), xmin, xmax);
-        float ynext = function(xnext);
+        fopen_s(&file, "out/out.csv", "w+t");
 
-        // take the new x if y next > ycurrent, or with some probability based on how much worse it is.
-        float A = ynext / ycurrent;
-        if (uniformDist(rng) < A)
+        float xcurrent = xstart;
+        float ycurrent = function(xstart);
+        float expectedValue = ycurrent;
+
+        fprintf(file, "\"index\",\"x\",\"y\",\"expected value\"\n");
+        fprintf(file, "\"%zu\",\"%f\",\"%f\",\"%f\"\n", size_t(0), xcurrent, ycurrent, expectedValue);
+
+        ymin = ycurrent;
+        ymax = ycurrent;
+
+        samples[0][0] = xcurrent;
+        samples[0][1] = ycurrent;
+
+        for (size_t sampleIndex = 1; sampleIndex < sampleCount; ++sampleIndex)
         {
-            xcurrent = xnext;
-            ycurrent = ynext;
+            float xnext = Clamp(xcurrent + normalDist(rng), xmin, xmax);
+            float ynext = function(xnext);
+
+            // take the new x if y next > ycurrent, or with some probability based on how much worse it is.
+            float A = ynext / ycurrent;
+            if (uniformDist(rng) < A)
+            {
+                xcurrent = xnext;
+                ycurrent = ynext;
+
+                if (ycurrent < ymin)
+                    ymin = ycurrent;
+
+                if (ycurrent > ymax)
+                    ymax = ycurrent;
+            }
+
+            expectedValue = Lerp(expectedValue, ycurrent, 1.0f / float(sampleIndex + 1));
+
+            samples[sampleIndex][0] = xcurrent;
+            samples[sampleIndex][1] = ycurrent;
+
+            fprintf(file, "\"%zu\",\"%f\",\"%f\",\"%f\"\n", sampleIndex, xcurrent, ycurrent, expectedValue);
+        }
+        fclose(file);
+    }
+
+    // make a histogram
+    Histogram histogram(xmin, xmax, 100);
+    for (const std::array<float, 2>& s : samples)
+        histogram.AddValue(s[0]);
+
+    // write out a histogram file
+    {
+        fopen_s(&file, "out/histogram.csv", "w+t");
+        fprintf(file, "\"Bucket Index\",\"Bucket X\",\"Actual Y\",\"Normalized Y\",\"Count\",\"Percentage\",\n");
+
+        // find out what the normalization constant is of the real function for the histogram buckets
+        float normalizationConstant = 0.0f;
+        for (size_t index = 0; index < histogram.m_numBuckets; ++index)
+        {
+            float percent = (float(index) + 0.5f) / float(histogram.m_numBuckets - 1);
+            float x = xmin + percent * (xmax - xmin);
+            float y = function(x);
+            normalizationConstant += y;
         }
 
-        expectedValue = Lerp(expectedValue, ycurrent, 1.0f / float(sampleIndex));
+        // print out the histogram data
+        for (size_t index = 0; index < histogram.m_numBuckets; ++index)
+        {
+            float percent = (float(index) + 0.5f) / float(histogram.m_numBuckets - 1);
+            float x = xmin + percent * (xmax - xmin);
+            float y = function(x);
 
-        fprintf(file, "\"%zu\",\"%f\",\"%f\",\"%f\"\n", sampleIndex, xcurrent, ycurrent, expectedValue);
-        histogram.AddValue(xcurrent);
+            fprintf(file, "\"%zu\",\"%f\",\"%f\",\"%f\",\"%zu\",\"%f\",\n", index, x, y, y / normalizationConstant, histogram.m_counts[index], float(histogram.m_counts[index]) / float(sampleCount));
+        }
+
+        fclose(file);
     }
-
-    // TODO: spit out values into a CSV and do a histogram with it?
-    fclose(file);
-
-    fopen_s(&file, "out/histogram.csv", "w+t");
-    fprintf(file, "\"Bucket Index\",\"Bucket X\",\"Actual Y\",\"Count\",\"Percentage\",\n");
-
-    for (size_t index = 0; index < histogram.m_numBuckets; ++index)
-    {
-        float percent = float(index) / float(histogram.m_numBuckets - 1);
-        float x = xmin + percent * (xmax - xmin);
-        float y = function(x);
-
-        fprintf(file, "\"%zu\",\"%f\",\"%f\",\"%zu\",\"%f\",\n", index, x, y, histogram.m_counts[index], float(histogram.m_counts[index])/float(sampleCount));
-    }
-
-    fclose(file);
 }
 
 float Sin(float x)
@@ -113,10 +145,15 @@ float Sin(float x)
     return sinf(x);
 }
 
+float IndefiniteIntegral_Sin(float x)
+{
+    return -cosf(x);
+}
 
 int main(int argc, char** argv)
 {
-    MetropolisMCMC(Sin, 0.0f, 1.0f, 0.5f, 0.1f, 100000);
+
+    MetropolisMCMC(Sin, 0.0f, 3.5f, 0.5f, 0.1f, 100000);
 
     return 0;
 }
@@ -125,10 +162,16 @@ int main(int argc, char** argv)
 
 TODO:
 
-* Clamp to range instead of putting f in function.
+* i think clamping X biases things.
+ * I think instead, you should have it just return zero outside of where you want to go? try it and see.
+
+
 * Also make smaller step size! Maybe be based on range?
+ * need to play around with step sizes
+
 * average y value is the expected value.
  ? i did this one but it doesn't seem to be correct! (or is it?)
+ ? is expected value the x or the y in this case?
 
 * in the histogram, the actual calculated y's sum up to BUCKET_COUNT * 0.45970 (integration of sin(x) from 0 to 1).
  * the counts sum up to sample count, which can be normalized to a percentage, but if we are looking for a mean, what do we do?
@@ -159,6 +202,12 @@ TODO:
  
 
 
+ Notes:
+ * if function goes negative, I don't think it ever chooses probability values there.  Experiment and understand.
+ * I think clamping biases things. maybe makes it not symetric? i dunno.
+
+
+
  Links:
  - real good! https://stephens999.github.io/fiveMinuteStats/MH_intro.html
 
@@ -172,6 +221,8 @@ https://towardsdatascience.com/a-zero-math-introduction-to-markov-chain-monte-ca
 https://jeremykun.com/2015/04/06/markov-chain-monte-carlo-without-all-the-bullshit/
 
 
+
 Next: read up on metropolis light transport
+Next: Hamilton Monte Carlo since you can do big jumps if you can get derivative (dual numbers!)
 
  */
